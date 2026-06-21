17e578ab03376237808d191588b3a459380fd65e
Aurino Junior <aurinoaj@gmail.com>
2026-06-21 12:57:56 -0300

    chore: update layout dark mode and dropdown styling

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -13,7 +13,7 @@ export default async function AppLayout({ children }: { children: React.ReactNod
   return (
     <div className="flex flex-1">
       <AppSidebar />
-      <div className="flex flex-1 flex-col">
+      <div className="flex flex-1 flex-col min-w-0">
         <Topbar user={session.user} />
         <main className="flex-1 px-6 pb-8">{children}</main>
       </div>
